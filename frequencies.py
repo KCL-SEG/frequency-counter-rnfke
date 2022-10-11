e57cc9c8732644c756a2ee0c040a1a53b1d5b72f
Varishdan Kumariah <varishdan.kumariah@kcl.ac.uk>
2022-10-11 19:22:19 +0100

    done

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,11 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
+from collections import Counter
 def frequencies(items):
     frequencies = {}
+    newlist = []
+    for i in items:
+        newlist.append(str(i))
+
     # Your code goes here
-    return frequencies
+    return Counter(newlist)
